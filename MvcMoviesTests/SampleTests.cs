934f54f8f9de39572766ec2bcdf9ad4624ff5ba9
Badsauce <ninjazoby@gmail.com>
2014-04-01 08:29:25 -1000

    added tests

---
+++ MvcMoviesTests/SampleTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MvcMoviesTests
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void Hello()
+        {
+            string testString = "hello";
+
+            StringAssert.AreEqualIgnoringCase(testString, "HELLO");
+        }
+
+        [Test]
+        public void Math()
+        {
+            Assert.AreEqual(3 + 4, 7);
+        }
+    }
+}
